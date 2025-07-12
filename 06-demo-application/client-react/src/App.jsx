8aee0deac880df35f18f29b2b6f3bd4832dd9d44
vigneshs-dev <vigneshkavery97@gmail.com>
2025-07-12 14:11:57 +0530

    Added my name in Dev branch frontend

--- 06-demo-application/client-react/src/App.jsx
+++ 06-demo-application/client-react/src/App.jsx
@@ -34,7 +34,7 @@ function CurrentTime(props) {
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Hi Team! 👋</h1>
+      <h1>Hi Team! 👋 I am Vicky from Dev </h1>
       <CurrentTime api="/api/golang/" />
       <CurrentTime api="/api/node/" />
       <ReactQueryDevtools initialIsOpen={false} />
